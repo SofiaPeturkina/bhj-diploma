9f3397106411cff35a8be2dd4f615844df6bdae5
Sofia Peturkina <peturkina@bk.ru>
2022-02-02 16:34:48 +0300

    Second commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,33 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    const formData = new FormData();
+    xhr.responseType = "json";
 
+    let address = "?";
+    for (key in options.data) {
+        address += key + "=" + options.data[key] + "&";
+    }
+    
+    address = address.slice(0, -1);
+    for (key in options.data) {
+        formData.append(key, options.data[key]);
+    }
+    
+    try {
+        if (options.method === "GET") {
+            xhr.open(options.method, options.url + address);
+            xhr.send(formData);
+        }
+    } catch (err) {
+        options.callback(err);
+    }
+
+    xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+            let response = xhr.response;
+            options.callback(null, response);
+        }
+    });
 };
